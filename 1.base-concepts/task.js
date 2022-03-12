4accd81ceb8921cd900f8a77ea35e9d20b7aed99
Анастасия Внукова <nastenka696945@gmail.com>
2022-03-12 20:10:17 +0300

    First homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,18 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
+  let arr =[];
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr[0] = -b/(2*a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
-
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
