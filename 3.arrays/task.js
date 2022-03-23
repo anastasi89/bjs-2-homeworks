ae16ba3fda8ac3fd75caa5070561c93a82902b97
Анастасия Внукова <nastenka696945@gmail.com>
2022-03-23 19:56:36 +0300

    HW3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = arr1.length == arr2.length && arr1.every((element, i) => arr2[i] === element);
   // Ваш код
-
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let resultArr = arr.filter((element) => element > 0).filter((element) => element % 3 === 0).map((element) => element * 10)
   // Ваш код
 
   return resultArr; // array
