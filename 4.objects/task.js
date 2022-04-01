f71b23d446162a9aaee217a0c28341edd404ee23
Анастасия Внукова <nastenka696945@gmail.com>
2022-04-01 17:27:27 +0300

    HW4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
     // Ваш код
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
   //ваш код
 }
 
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){
+        this.marks = [mark];
+        } else {
+            this.marks.push(mark);
+        }
+}
+
+Student.prototype.addMarks = function (...marks) {
+    this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (let mark of this.marks) {
+        sum += 1;
+    }
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
 // ваш код для остальных методов
