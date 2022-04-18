978326e9f4d1620df9a3c0dc5c1658f8ea0a154c
Анастасия Внукова <nastenka696945@gmail.com>
2022-04-18 19:46:29 +0300

    HW7

---
+++ 7.async/task.js
@@ -0,0 +1,62 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+
+    addClock(time, callback, id) {
+        if (!id) {
+            throw new Error("Ошибка! Параметр не передан");
+        }
+        if (this.alarmCollection.some(alarm => alarm.id === id)) {
+        	console.error("Звонок уже существует");
+        	return;
+        }
+        this.alarmCollection.push({id: id, time: time, callback: callback});
+    }
+
+    removeClock(id) {
+    	let alarmCollectionFull = this.alarmCollection.length;
+    	let alarmCollectionFilter = this.alarmCollection.length;
+    	this.alarmCollection = this.alarmCollection.filter(alarm => alarm.id !== id);
+    	return alarmCollectionFull > alarmCollectionFilter;
+    }
+
+    getCurrentFormattedTime() {
+    	return new Date().toLocaleTimeString("ru-Ru", {
+    		hour: "2-digit",
+    		minute: "2-digit",
+    	});
+    }
+
+    start() {
+    	const checkClock = (alarm) => {
+    		if (alarm.time === this.getCurrentFormattedTime()) {
+    			alarm.callback();
+    		}
+    	};
+    	if (!this.timerId) {
+    		this.timerId = setInterval(() => {
+    			this.alarmCollection.forEach(alarm => checkClock(alarm));
+    		}, 1000);
+    	}
+    }
+
+    stop() {
+    	if (!this.timerId) {
+    		clearInterval(this.timerId);
+    		this.timerId = null;
+    	}
+    }
+
+    printAlarms() {
+    	this.alarmCollection.forEach(alarm => {
+    		console.log(`Будильник №${alarm.id} заведен на ${alarm.time}`);
+    	});
+    }
+
+    clearAlarms() {
+    	this.stop();
+    	this.alarmCollection = [];
+    }
+}
